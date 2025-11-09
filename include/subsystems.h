5914afe32489e58d485866ffd400b5d70c72bdf9
yaboi332 <ethanfu868@gmail.com>
2025-11-08 20:25:12 -0500

    Revert "Styling drive"

--- include/subsystems.h
+++
@@ -1,6 +0,0 @@
-#include "main.h"
-
-extern pros::Controller controller;
-
-extern pros::MotorGroup leftMotors;
-extern pros::MotorGroup rightMotors;
--- src/subsystems.cpp
+++
@@ -1,7 +0,0 @@
-#include "main.h"
-#include "constants.h"
-
-pros::Controller controller(pros::E_CONTROLLER_MASTER);
-
-pros::MotorGroup leftMotors(LEFT_DRIVE_WHEEL_PORTS);
-pros::MotorGroup rightMotors(RIGHT_DRIVE_WHEEL_PORTS);
